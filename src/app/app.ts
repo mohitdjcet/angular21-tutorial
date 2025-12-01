5ded6e8f3ffc48966b30b2bc7493f19c83502e4b
Mohit Kumar <mks.djcet@gmail.com>
2025-12-01 15:41:26 +0530

    updated code

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+  count = 0;
 
+  handleClick(){
+    let abc="Hi there";
+    console.log("Button clicked", abc,this.count);
+    this.helloWorld();
+  }
+
+  helloWorld() {
+    console.log("Hello World");
+  }
 }
