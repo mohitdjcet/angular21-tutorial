0bc14290cf9a45e39a7931bbdf3e14ee4c55c055
Mohit Kumar <mks.djcet@gmail.com>
2025-12-01 16:13:33 +0530

    Updated code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 @Component({
   selector: 'app-root',
   imports: [],
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+  // value = 10;
 
+  // value= "Mohit"
+
+  name: string | number = 20;
+  age: number = 28;
+  isValid: boolean = true;
+
+  handleClick() {
+    let value;
+    value = "Mohit Kumar"
+    value = 10;
+    console.log(value);
+  }
+
+  sum(a:number, b:number){
+    console.log(a + b);
+  }
+
+   count = signal(0);
+
+  updateCount(value: number): void {
+    this.count.update(c => c + value);
+  }
 }
