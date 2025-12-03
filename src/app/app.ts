8b2adf2a8ef75f1fe1fafb868efc35827eda2307
Mohit Kumar <mks.djcet@gmail.com>
2025-12-03 16:24:11 +0530

    Updated event code

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+  handleClick() {
+    console.log('Button clicked!');
+  }
 
+  onTyping(event: any){
+    console.log("Typed", event.target.value);
+  }
+
+  onKeyUp(event: any){
+    console.log("Key Up Event", event.key);
+  }
+
+  onHover(){
+    console.log("Hover Event");
+  }
+
+  onLeave(){
+    console.log("Leave Event");
+  }
+
+  onBlur(){
+    console.log("Blur Event");
+  }
+
+  onFocus(){
+    console.log("Focus Event");
+  }
 }
