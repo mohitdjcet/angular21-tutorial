a8b627ee8b6466dad85c6f9f4ea562e30a745a10
Mohit Kumar <mks.djcet@gmail.com>
2025-12-03 15:58:30 +0530

    Updated code

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+  count : number = 0;
 
+  // handleAdd(){
+  //   this.count++;
+  // }
+  // handleMinus(){
+  //   if(this.count > 0){
+  //     this.count--;
+  //   }
+  // }
+  // handleReset(){
+  //   this.count = 0;
+  // }
+
+  handle(value: string){
+    if(value === 'plus'){
+      this.count++;
+    }
+    else if (value === 'minus'){
+      this.count--;
+    }
+    else if (value === 'reset'){
+      this.count = 0;
+    }
+  }
 }
