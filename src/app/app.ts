158d732c6a86d410efef323d4c9cf3f42065f367
Mohit Kumar <mks.djcet@gmail.com>
2025-12-04 15:31:32 +0530

    Updated code

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+  name: string = "";
+  city: string = "";
+  email: string = "";
 
+  updateName(val:string){
+    this.name = val;
+  }
+
+  getEmail(val:string){
+    this.email = val;
+  }
 }
