0aa08c7ab652f5451aa39c5199a1eee6ded57fd2
Mohit Kumar <mks.djcet@gmail.com>
2025-12-05 15:43:20 +0530

    Updated code

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+  isLoggedIn = true;
 
+  marks = 65;
+
+  showBox = true;
+
+  toggleBox() {
+    this.showBox = !this.showBox;
+  }
+
+  age = 0;
+
+  updateAge(val: string) {
+    this.age = Number(val);
+  }
 }
