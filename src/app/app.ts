43fd4a5960b70008a573b812ca5a52657ade1e56
Mohit Kumar <mks.djcet@gmail.com>
2025-12-05 16:28:40 +0530

    Updated code

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+  value: string = 'contact';
 
+  tab='home';
+
+  categories = '';
+  updateCat(val: string) {
+    this.categories = val.toLowerCase();
+  }
 }
