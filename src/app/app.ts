ae4d6f8e97161700589410cecbea901fdc00a4e9
Mohit Kumar <mks.djcet@gmail.com>
2025-12-05 12:41:08 +0530

    Updated code

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,8 +2,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   imports: [],
+  // template: `<h2 class="box">Welcome to the App!</h2>`,
+  // styles: [`
+  //   .box{
+  //     background-color: red;
+  //     padding: 20px;
+  //     text-align: center;
+  //   }`]
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css', './buttons.css']
 })
 export class App {
 
