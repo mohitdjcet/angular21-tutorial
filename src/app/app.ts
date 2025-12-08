42f0717908f60b9eccf025ccf65c123c1686d1a8
Mohit Kumar <mks.djcet@gmail.com>
2025-12-08 19:06:31 +0530

    Updated code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 @Component({
   selector: 'app-root',
   imports: [],
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+  count = signal(0); //Signal Created
+  val = 20;
 
+  // count.update(c => c + 1); //Signal Updated
+
+  // items.mutate( a => a.push(4) ); //Signal Mutated
+
+  increment(){
+    this.count.update(c => c + 1);
+  }
+  decement(){
+    this.count.update(c => c - 1);
+  }
+  reset(){
+    this.count.set(0);
+  }
 }
