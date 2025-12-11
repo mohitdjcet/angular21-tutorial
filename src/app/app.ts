41aee0aef331a9aebfb38d1b7935cf3698002dd0
Mohit Kumar <mks.djcet@gmail.com>
2025-12-11 19:55:51 +0530

    Updated code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 @Component({
   selector: 'app-root',
   imports: [],
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+  // count = signal(0);
 
+  // constructor(){
+  //   effect(()=>{
+  //     console.log("count Value:", this.count())
+  //   });
+  // }
+
+  // increment(){
+  //   this.count.update(c => c + 1);
+  // }
+
+  // isDarkMode = signal(false);
+
+  // constructor(){
+  //   effect(()=>{
+  //     if(this.isDarkMode()){
+  //       document.body.style.backgroundColor = 'black';
+  //       document.body.style.color = 'white';
+  //     }
+  //     else{
+  //       document.body.style.backgroundColor = 'white';
+  //       document.body.style.color = 'black';
+  //     }
+  //   });
+  // }
+
+  // toggle(){
+  //   this.isDarkMode.update(mode => !mode);
+  // }
+
+  message = signal("");
+
+  constructor(){
+    effect(()=>{
+      if(this.message()){
+        setTimeout(() => {
+          this.message.set("");
+        }, 2000);
+      }
+    });
+  }
+
+  showMessage(){
+    this.message.set("Hello, this is a temporary message!");
+  }
 }
