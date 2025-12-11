1adcbde1590335075d5c0b94d96037d75bb54ba3
Mohit Kumar <mks.djcet@gmail.com>
2025-12-11 14:07:41 +0530

    Updated code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface User {
+  name: string;
+  age: number;
+}
 @Component({
   selector: 'app-root',
   imports: [],
@@ -6,5 +11,21 @@ import { Component } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+  count = signal<number>(0);
+  name = signal<string>('Angular');
+  isAdmin = signal<boolean>(true);
 
+  numbers = signal<number[]>([1,20,25,30]);
+  user = signal<User>({
+    name: 'Mohit',
+    age: 24
+  })
+
+  directUpdate(){
+    this.numbers.set([100,200,300]);
+  }
+
+  addNumber(){
+    this.numbers.update(arr => [...arr,400])
+  }
 }
