7f779cbb2a53300d257d3f0c0c286e1b9b319110
Mohit Kumar <mks.djcet@gmail.com>
2025-12-12 13:17:46 +0530

    Updated code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+ lName = "mohit"
+ uName = "MOHIT"
 
+ today = new Date();
+
+ amount = 2500;
+
+ per= 0.35;
+
+ user = {
+  name: 'Mohit',
+  age: 25
+ }
 }
