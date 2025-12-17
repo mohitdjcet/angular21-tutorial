072211135c5a0c2e4b5cdf9a60d597f72ca65232
Mohit Kumar <mks.djcet@gmail.com>
2025-12-17 15:24:58 +0530

    Updated code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 @Component({
   selector: 'app-root',
   imports: [],
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+  // count = signal(0);
+  // name = signal('Angular');
+
+  // setCount(){
+  //   this.count.set(2);
+  // }
+
+  count = signal(2);
+  price = signal(5);
+  // doubleCount = computed(() => this.count() * 2);
+
+  total = computed(() => this.count() * this.price());
+  constructor(){
+    effect(() =>{
+      console.log("Total:", this.count());
+    })
+  }
+
+  upateCount(){
+    this.count.update(c => c + 1);
+  }
+
+  upatePrice(){
+    this.price.update(p => p + 1);
+  }
 
 }
