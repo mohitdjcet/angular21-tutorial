f48cde0af28719e1ba2a100a5d923177ae12f2b9
Mohit Kumar <mks.djcet@gmail.com>
2025-12-21 21:28:23 +0530

    Updated code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,signal } from '@angular/core';
 @Component({
   selector: 'app-root',
   imports: [],
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+  // count = signal(0);
+  // isLoggedIn = signal(false);
 
+  // items = signal<string[]>([
+  //   "Angular",
+  //   "React",
+  //   "Vue"
+  // ])
+
+  // addItem() {
+  //   this.items.update( items => [...items, "HTML"] );
+  // }
+
+  role = signal< 'admin' | 'user' >('user');
+
+  features = signal<string[]>([
+    "Dashboard",
+    "Profile",
+    "Settings"
+  ]);
+
+  makeAdmin() {
+    this.role.set('admin');
+    this.features.set([
+      "Dashboard",
+      "Profile",
+      "Settings",
+      "Admin Panel",
+      "User Management"
+    ]);
+  }
+
+  makeUser() {
+    this.role.set('user');
+    this.features.set([
+      "Dashboard",
+      "Profile",
+      "Settings"
+    ]);
+  }
 }
