9b84d42d63f7102abfd36e80758e3ea3b6d940fa
Mohit Kumar <mks.djcet@gmail.com>
2025-12-22 14:01:19 +0530

    Updated code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { email } from '@angular/forms/signals';
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  // name = signal('');
+  // email = signal('');
 
+  // submitForm(){
+  //   console.log(`Name: ${this.name()}, Email: ${this.email()}`);
+  // }
+
+  userSignal = signal({ name: '', email: '' });
+
+  form : any;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: [''],
+      email: ['']
+    })
+
+    this.form.valueChanges.subscribe((value: any) => {
+      this.userSignal.set(value);
+    });
+  }
+
+  submitForm() {
+    console.log(`Name: ${this.userSignal().name}, Email: ${this.userSignal().email}`);
+  }
 }
