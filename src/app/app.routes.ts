26ff0dc3254bf57bfcf99848d2011b61ae9a2831
Mohit Kumar <mks.djcet@gmail.com>
2025-12-27 15:51:11 +0530

    Updated code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: Home },
+  {
+    path: 'admin',
+    loadComponent: () => import('./admin/admin').then(m => m.Admin)
+  },
+];
