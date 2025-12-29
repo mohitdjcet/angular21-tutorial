52932099a8bc53bd39b821667f2e7eeb9351112a
Mohit Kumar <mks.djcet@gmail.com>
2025-12-29 12:04:18 +0530

    Updated code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ProductComp } from './product-comp/product-comp';
+import { ProductsComp } from './products-comp/products-comp';
 
 export const routes: Routes = [
-    { path: 'product/:id', component: ProductComp}
+    { path: 'product/:id', component: ProductComp},
+    { path: 'products', component: ProductsComp}
 ];
