29c81ec2d7c78d29227f12d4996e7e2bc4237df0
Mohit Kumar <mks.djcet@gmail.com>
2025-12-30 21:11:39 +0530

    Updated code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Dashboard } from './dashboard/dashboard';
+import { Product } from './product/product';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login', component: Login},
+    {path:'dashboard', component: Dashboard},
+    {path:'product/:id', component:Product},
+    {path:'', redirectTo:'login', pathMatch:'full'}
+];
