388b7ef50c36eadb268da4f4788ebc379045e737
Mohit Kumar <mks.djcet@gmail.com>
2026-01-02 18:04:57 +0530

    Updated code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 @Component({
   selector: 'app-root',
   imports: [],
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+  users: any[] = [];
 
+  constructor(private userService: User){}
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe((data: any)=>{
+      this.users = data;
+      console.log(this.users,'users data');
+    })
+  }
 }
