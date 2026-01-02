29fc602116f34211ee72aa5bca89c1cff275bb3b
Mohit Kumar <mks.djcet@gmail.com>
2026-01-02 17:47:37 +0530

    Updated code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { Field, form } from '@angular/forms/signals';
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule, Field],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  //Form model based on signals
+  loginModel = signal({
+    email: '',
+    password: ''
+  })
 
+  //Create form from signal Model
+  loginForm = form(this.loginModel);
+
+  submit() {
+    console.log(this.loginModel());
+  }
 }
